085bcab5337ebf40f8a6838f75e4b6a933136886
Ibrahem Alahdal <ibralah@ut.utm.edu>
2017-02-01 17:27:13 -0600

    Added some php to ready for the sort function

--- Stats.php
+++ Stats.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-<form action="Stats.php" method="post">
-Title: <input type="text" name="FirstName" value=""><br>
-Chart Type: <select>
-  <option value="Stats">Stats</option>
-  <option value="Bar">Bar</option>
-  <option value="Line">Line</option>
-  <option value="Pie">Pie</option>
-</select><br>
-Sort by: <select>
-  <option value="None">None</option>
-  <option value="Score">Score</option>
-  <option value="Name">Name</option>
-  <option value="Last Name">Last</option>
-</select><br>
-Chart Data: <textarea rows="5" cols="50">Bob Bradley, 99 </textarea>
-
-<input type="submit" value="Submit">
-</form>
-
-<h1> <!-- This is where the heading is echo back -->
-	<br>
-	<?php echo $_POST["FirstName"]; ?>
-	<br>
-</h1>
-
-</body>
+  <body>
+    
+    <form action="Stats.php" method="post">
+      Title: <input type="text" name="FirstName" value=""><br>
+      Chart Type: <select>
+	<option value="Stats">Stats</option>
+	<option value="Bar">Bar</option>
+	<option value="Line">Line</option>
+	<option value="Pie">Pie</option>
+      </select><br>
+      Sort by: <select>
+	<option value="None">None</option>
+	<option value="Score">Score</option>
+	<option value="Name">Name</option>
+	<option value="Last Name">Last</option>
+      </select><br>
+      Chart Data: <textarea rows="5" cols="50">Bob Bradley, 99 </textarea>
+      
+      <input type="submit" value="Submit">
+    </form>
+    
+    <HR>
+    
+    <h1> <!-- This is where the heading is echo back -->
+      <br>
+      <?php echo $_POST["FirstName"]; ?>
+      <br>
+    </h1>
+    
+    <?php 
+       $str1="Highest: ";
+       $str2="Lowest: ";
+       $str3="Average: ";
+       
+       echo $_POST["str1"];
+       echo "<br>";
+       echo $_POST["str2"];
+       echo "<br>";
+       echo $_POST["str3"];
+       ?>
+    
+  </body>
 </html>
