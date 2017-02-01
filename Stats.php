9149a41bd03bacc1e7e5fce074c9999b5ddd3943
Cody Thomason <Lion@Codys-MacBook-Pro.local>
2017-02-01 15:57:14 -0600

    Added text boxes
    Title

---
+++ Stats.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="bob_form.asp">
+Title: <input type="text" name="FirstName" value=""><br>
+Chart Type: <select>
+  <option value="Stats">Stats</option>
+  <option value="Bar">Bar</option>
+  <option value="Line">Line</option>
+  <option value="Pie">Pie</option>
+</select><br>
+Sort by: <select>
+  <option value="None">None</option>
+  <option value="Score">Score</option>
+  <option value="Name">Name</option>
+  <option value="Last Name">Last</option>
+</select><br>
+Chart Data: <textarea rows="5" cols="50">Bob Bradley, 99 </textarea>
+
+<input type="submit" value="Submit">
+</form>
+
+
+</body>
+</html>
