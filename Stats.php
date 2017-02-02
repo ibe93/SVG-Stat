b3ed5a80594f90ed7f381253b3fba8ae37bd7334
Ruchik Patel <rucpate@ut.utm.edu>
2017-02-01 19:45:16 -0600

    New Layout
    Couldn’t come up with any better color

--- Stats.php
+++ Stats.php
@@ -1,31 +1,59 @@
 <!DOCTYPE html>
 <html>
 <body>
+<style>
+#text 
+   {
+   font-size: 100%;
+   display: inline;
+   font-size: 40%;
+   color: black;
+   padding: 20px 40px;
+   text-decoration: none;
+   border-radius: 4px 4px 0 0;
+   background-color: white;
+   cursor: pointer; 
+   border-radius: 50%
+   }   
+</style>
+
+<div style="float: center; position: relative;  background-color: lightgrey; height: 383px; width: 1155px; border: 20px; padding: 10px; margin: 25px;">
 
 <form action="Stats.php" method="post">
-Title: <input type="text" name="FirstName" value=""><br>
-Chart Type: <select>
-  <option value="Stats">Stats</option>
-  <option value="Bar">Bar</option>
-  <option value="Line">Line</option>
-  <option value="Pie">Pie</option>
-</select><br>
+
+  <div style="font-size: 170%">Title:<input style="font-size: 90%;" type="text" name="FirstName" value=""></div><br><hr>
+
+  <div style="font-size: 150%">
+    Chart Type: <select>
+    <option value="Stats">Stats</option>
+    <option value="Bar">Bar</option>
+    <option value="Line">Line</option>
+    <option value="Pie">Pie</option>
+  </select><br><hr>
+
 Sort by: <select>
   <option value="None">None</option>
   <option value="Score">Score</option>
   <option value="Name">Name</option>
   <option value="Last Name">Last</option>
-</select><br>
-Chart Data: <textarea rows="5" cols="50">Bob Bradley, 99 </textarea>
+</select><br><hr>
 
-<input type="submit" value="Submit">
+Chart Data: <textarea action="Stats.php" method="post" name="comment" rows="5" cols="50"></textarea> </div>
+<br><hr>
+<input id="text"  type="submit" value="Submit">
+<br><br><hr>
 </form>
 
-<h1> <!-- This is where the heading is echo back -->
+<h1> <!--This is where the heading is echo back -->
+	
 	<br>
 	<?php echo $_POST["FirstName"]; ?>
 	<br>
+
 </h1>
+
+</div>
+
 
 </body>
 </html>
