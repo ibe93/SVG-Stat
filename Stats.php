0865afb09ddc24e6a320249d484cdcfa38ee4751
Cody Thomason <Lion@Codys-MacBook-Pro.local>
2017-02-10 12:38:01 -0600

    Added the different Sorts
    Most stuff should be working. About to add in SVG

--- Stats.php
+++ Stats.php
@@ -25,7 +25,7 @@
   <div style="font-size: 170%">Title:<input style="font-size: 90%;" type="text" name="FirstName" value=""></div><br><hr>
 
   <div style="font-size: 150%">
-    Chart Type: <select>
+    Chart Type: <select name="chartType">
     <option value="Stats">Stats</option>
     <option value="Bar">Bar</option>
     <option value="Line">Line</option>
@@ -74,6 +74,13 @@ Chart Data: <textarea action="Stats.php" name="comment" rows="5" cols="50"></tex
       $lastName[] = $last_Name;
       $counter++;
 	}
+
+	array_multisort($grade, $firstName, $lastName);
+	$tempSize = sizeof($grade) - 1;
+	$zero = 0;
+	echo "Lowest Score: $firstName[$zero] ===>$lastName[$zero] ======> $grade[$zero]" . '<br>';
+	echo "Highest Score: $firstName[$tempSize] ===>$lastName[$tempSize] ======> $grade[$tempSize]" . '<br>';
+
   }
 
 
@@ -81,13 +88,39 @@ Chart Data: <textarea action="Stats.php" name="comment" rows="5" cols="50"></tex
 	 {
 	 	//echo "Working";
 
-	array_multisort($grade[2], $firstName, $lastName);
+	array_multisort($grade, $firstName, $lastName);
 	 	//sort($grage[3]);
 	 for ($i=0; $i <count($grade) ; $i++)
       {
         echo "$firstName[$i] ===>$lastName[$i] ======> $grade[$i]" . '<br>';
       }
-}
+	} elseif($_POST['sortStuff'] == "Name")
+	 {
+	 	//echo "Working";
+
+	array_multisort($firstName, $grade, $lastName);
+	 	//sort($grage[3]);
+	 for ($i=0; $i <count($grade) ; $i++)
+      {
+        echo "$firstName[$i] ===>$lastName[$i] ======> $grade[$i]" . '<br>';
+      }
+	} elseif($_POST['sortStuff'] == "Last Name")
+	 {
+	 	//echo "Working";
+
+	array_multisort($lastName, $firstName, $grade);
+	 	//sort($grage[3]);
+	 for ($i=0; $i <count($grade) ; $i++)
+      {
+        echo "$firstName[$i] ===>$lastName[$i] ======> $grade[$i]" . '<br>';
+      }
+	}
+
+
+	if($_POST['chartType'] == "Bar")
+	{
+		
+	}
 ?>
 
 
@@ -99,94 +132,7 @@ Chart Data: <textarea action="Stats.php" name="comment" rows="5" cols="50"></tex
         echo "$firstName[$i] ===>$lastName[$i] ======> $grade[$i]" . '<br>';
       }-->
 
-</body>
-=======
-  <body>
-    <style>
-      #text 
-      {
-      font-size: 100%;
-      display: inline;
-      font-size: 40%;
-      color: black;
-      padding: 20px 40px;
-      text-decoration: none;
-      border-radius: 4px 4px 0 0;
-      background-color: white;
-      cursor: pointer; 
-      border-radius: 50%
-      }   
-    </style>
-    
-    <div style="float: center; position: relative;  background-color: lightgrey; height: 383px; width: 1155px; border: 20px; padding: 10px; margin: 25px;">
-      
-      <form action="Stats.php" method="post">
-	
-	<div style="font-size: 170%">Title:<input style="font-size: 90%;" type="text" name="FirstName" value=""></div><br><hr>
-	
-	<div style="font-size: 150%">
-	  Chart Type: <select>
-	    <option value="Stats">Stats</option>
-	    <option value="Bar">Bar</option>
-	    <option value="Line">Line</option>
-	    <option value="Pie">Pie</option>
-	  </select><br><hr>
-	  
-	  Sort by: <select name="selectSort">
-	    <option value="None">None</option>
-	    <option value="Score">Score</option>
-	    <option value="Name">Name</option>
-	    <option value="Last Name">Last</option>
-	  </select><br><hr>
-	  
-	  Chart Data: <textarea action="Stats.php" method="post" name="comment" rows="5" cols="50"></textarea> </div>
-	<br><hr>
-	<input id="text"  type="submit" value="Submit">
-	<br><br><hr>
-      </form>
-      
-      <br>
-      <h1 style="align: centr;"> <!--This is where the heading is echo back -->
-	<?php echo $_POST["FirstName"]; ?>
-	<br>
-      </h1>
-      
-      <?php   	
-	 $str = $_POST["comment"]; // Assigning Post to variable str
-	 
-	 $final_grade = [];
-	 $name = [];
-	 $firstName = [];
-	 $lastName = [];
-	 
-	 if($_POST) // Loop until the post is finish
-	 {
-	 foreach (explode("\n", $str) as $key) 
-	 {
-	 list($name, $final_grade) = explode(',', $key);
-	 list($firstName, $lastName) = explode(" ", $name);
-	 }
-	 }
-	 function sorter(&$final_grade, &$firstName, &$lastName)
-	 {
-	 array_multisort($final_grade, $firstName, $lastName);
-	 }
-	 
- 	 if($_POST["selectSort"] == "Score")
-	 {
-	 sorter($final_grade, $firstName, $lastName);
-	 
-	 for($i = 0; $i < count($final_grade); $i++)
-			  {
-			  echo "$final_grade ===> $firstName ===> $lastName";
-			  echo "<br>";
-			  }
-			  }
-			  
-			  ?>
-	 
-	 </body>    
->>>>>>> origin/master
+
 </html>
 
 
---
+++ upload.php
@@ -0,0 +1,30 @@
+
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Onlu allow csv files
+if($fileType != "csv" ) {
+    echo "File must be a csv.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} 
+else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "CRITICAL ERROR: ABORT ABORT. FILE NOT UPLOADED!";
+    }
+}
+?>
